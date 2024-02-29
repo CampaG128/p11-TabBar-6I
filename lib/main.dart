97f1442d972b66857451a3df222dcaa5a32b6d4b
GREGORIO_CAMPA_GAL2 <a.21308051281175@cbtis128.edu.mx>
2024-02-29 18:55:22 +0000

    Actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Learning',
       theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+// Stateful
+
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+} // MiPaginaInicial
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Tabbar Gregorio Campa"),
+          centerTitle: true,
+          bottom: TabBar(tabs: [
+            Tab(
+              text: "Playeras",
+              icon: Icon(Icons.call_missed_outgoing_rounded),
+            ),
+            Tab(
+              text: "Polos",
+              icon: Icon(Icons.cabin),
+            ),
+            Tab(
+              text: "Gorras",
+              icon: Icon(Icons.dangerous_rounded),
+            ),
+            Tab(
+              text: "Ofertas",
+              icon: Icon(Icons.campaign_rounded),
+            ),
+          ]), // Fin de tabs // Fin bottom tabbar
         ),
-      ),
-    );
-  }
-}
+        body: TabBarView(
+          children: const <Widget>[
+            Center(
+              child: Text("Ferrari",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            ),
+            Center(
+              child: Text("Mclaren",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            ),
+            Center(
+              child: Text("Mercedes",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            ),
+            Center(
+              child: Text("Aston Martin",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            ),
+          ], // Jardin de niños
+        ), // Fin del tabbarview
+      ), // Fin de scaffold
+    ); // DefaultTabController
+  } // Fin widgets
+} // _MiPaginaInicialState
